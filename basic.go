16f5ba0d6685a6c131557ba3f4212ffbb6c1c36a
invi1998 <2267661826@qq.com>
2022-05-26 10:29:55 +0800

    枚举 enum

--- basic.go
+++ basic.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+)
 
 func variable() {
 	var a int
@@ -21,7 +25,61 @@ func variableTypeDeduction() {
 
 func variableShorter() {
 	a, b, c, s := 3, 4.33, true, "def"
-	fmt.Printf("%T, %T, %T, %T", a, b, c, s)
+	fmt.Printf("%T, %T, %T, %T\n", a, b, c, s)
+}
+
+//欧拉公式
+func euler() {
+	fmt.Println(cmplx.Pow(math.E, 1i*math.Pi) + 1)
+}
+
+func triangle() {
+	var a, b int = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(c)
+}
+
+func consts() {
+	const filename = "abc.txt"
+	const a, b = 3, 4
+	var c int
+	c = int(math.Sqrt(float64(a*a + b*b)))
+	fmt.Println(filename)
+	fmt.Println(c)
+}
+
+func enums() {
+	//普通枚举类型
+	const (
+		red   = 0
+		green = 1
+		blue  = 2
+	)
+
+	//自增型枚举值
+	fmt.Println(red, green, blue)
+
+	const (
+		cpp = iota
+		_
+		python
+		golang
+		javascript
+	)
+
+	fmt.Println(cpp, python, golang, javascript)
+
+	//b, kb, mb, gb, tb, pb
+	const (
+		b = 1 << (10 * iota)
+		kb
+		mb
+		gb
+		tb
+		pb
+	)
+	fmt.Println(b, kb, mb, gb, tb, pb)
 }
 
 func main() {
@@ -30,4 +88,8 @@ func main() {
 	variableInitialValue()
 	variableTypeDeduction()
 	variableShorter()
+	euler()
+	triangle()
+	consts()
+	enums()
 }
